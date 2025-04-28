8a6f31498c1ef598be44fdb1b90b0beff4cd356b
zenith <zenithyap88@gmail.com>
2025-04-28 13:28:13 +0800

    Add node class

---
+++ node.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor(data, left=null, right=null) {
+        this._data = data;
+        this._left = left;
+        this._right = right;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    set data(data) {
+        this._data = data;
+    }
+
+    get left() {
+        return this._left;
+    }
+
+    set left(leftNode) {
+        this._left = leftNode;
+    }
+
+    get right() {
+        return this._right;
+    }
+
+    set right() {
+        this._right = rightNode;
+    }
+}
